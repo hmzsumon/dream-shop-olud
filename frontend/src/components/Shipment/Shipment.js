49c818bab1a54b349d5bb3ea75e8354a0d28fd89
Zakaria Sumon <zsumonn@gmail.com>
2020-07-18 09:39:01 +0600

    db connect problem

--- frontend/src/components/Shipment/Shipment.js
+++ frontend/src/components/Shipment/Shipment.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import './Shipment.css';
 import { useAuth } from '../Login/useAuth';
 import { getDatabaseCart, processOrder } from '../../utilities/databaseManager';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from '../checkoutForm/checkoutForm';
 
 const Shipment = () => {
+  const [shipInfoAdded, setShipInfoAdded] = useState(false);
   const auth = useAuth();
   const { register, handleSubmit, errors } = useForm();
+  const stripePromise = loadStripe(
+    'pk_test_51H5aTtERG3pC1iKYYA7blXv14MVLsZlUY1BC8hexOrpzEWFdi09R3361Phnj4pS7Sls0fqySkiYdX1fZ7PtOCued00aq3Vb5wn'
+  );
 
   //submit handler
   const onSubmit = (data) => {
@@ -27,108 +34,159 @@ const Shipment = () => {
     })
       .then((res) => res.json())
       .then((order) => {
-        alert('Your Order Successfully Placed with order id:' + order._id);
-        processOrder();
+        setShipInfoAdded(true);
+        // alert('Your Order Successfully Placed with order id:' + order._id);
+        // processOrder();
       });
   };
 
   return (
-    <div className="shipment">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <ul className="shipment-form">
-          <li>
-            <input
-              name="name"
-              ref={register({ required: true })}
-              placeholder="name:"
-              defaultValue={auth.user.name}
-            />
-            {errors.name && (
-              <span className="ship-error">Name is required</span>
-            )}
-          </li>
-          <li>
-            <input
-              name="email"
-              ref={register({ required: true })}
-              placeholder="email:"
-              defaultValue={auth.user.email}
-            />
-            {errors.email && (
-              <span className="ship-error">Email is required</span>
-            )}
-          </li>
+    <div className="shipment-wrapper">
+      {/* START ORDER */}
+      <div
+        style={{ display: shipInfoAdded && 'none' }}
+        className="order-container"
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h3>Billing Address</h3>
+          <ul className="order-form">
+            <li>
+              <label htmlFor="name">
+                {' '}
+                <i className="fa fa-user" aria-hidden="true"></i> Full Nmae:
+              </label>
+              <input
+                name="name"
+                ref={register({ required: true })}
+                placeholder="name:"
+                defaultValue={auth.user.name}
+              />
+              {errors.name && (
+                <span className="ship-error">Name is required</span>
+              )}
+            </li>
+            <li>
+              <div>
+                <label htmlFor="email">
+                  {' '}
+                  <i className="fa fa-envelope" aria-hidden="true"></i> Email:
+                </label>
+                <input
+                  name="email"
+                  ref={register({ required: true })}
+                  placeholder="email:"
+                  defaultValue={auth.user.email}
+                />
+                {errors.email && (
+                  <span className="ship-error">Email is required</span>
+                )}
+              </div>
+              <div>
+                <label htmlFor="mobileNum">
+                  {' '}
+                  <i className="fa fa-phone" aria-hidden="true"></i> Number:
+                </label>
+                <input
+                  name="mobileNum"
+                  ref={register({ required: true })}
+                  placeholder="mobile number:"
+                />
+                {errors.mobileNum && (
+                  <span className="ship-error">Mobile Number is required</span>
+                )}
+              </div>
+            </li>
 
-          <li>
-            <input
-              name="addressLine1"
-              ref={register({ required: true })}
-              placeholder="addressLine1:"
-            />
-            {errors.addressLine1 && (
-              <span className="ship-error">Address Line 1 is required</span>
-            )}
-          </li>
+            <li>
+              <label htmlFor="addressLine1">
+                {' '}
+                <i className="fa fa-address-card" aria-hidden="true"></i>{' '}
+                Address1:
+              </label>
+              <input
+                name="addressLine1"
+                ref={register({ required: true })}
+                placeholder="addressLine1:"
+              />
+              {errors.addressLine1 && (
+                <span className="ship-error">Address Line 1 is required</span>
+              )}
+            </li>
 
-          <li>
-            <input
-              name="addressLine2"
-              ref={register}
-              placeholder="addresslin2:"
-            />
-          </li>
+            <li>
+              <label htmlFor="addressLine2">
+                {' '}
+                <i className="fa fa-address-card" aria-hidden="true"></i>{' '}
+                Address2 (Optional):
+              </label>
+              <input
+                name="addressLine2"
+                ref={register}
+                placeholder="addresslin2:"
+              />
+            </li>
 
-          <li>
-            <input
-              name="country"
-              ref={register({ required: true })}
-              placeholder="country:"
-            />
-            {errors.country && (
-              <span className="ship-error">Country is required</span>
-            )}
-          </li>
+            <li>
+              <label htmlFor="addressLine2">
+                {' '}
+                <i className="fa fa-globe" aria-hidden="true"></i> Country:
+              </label>
+              <input
+                name="country"
+                ref={register({ required: true })}
+                placeholder="country:"
+              />
+              {errors.country && (
+                <span className="ship-error">Country is required</span>
+              )}
+            </li>
 
-          <li>
-            <input
-              name="city"
-              ref={register({ required: true })}
-              placeholder="city:"
-            />
-            {errors.city && (
-              <span className="ship-error">City is required</span>
-            )}
-          </li>
+            <li>
+              <div>
+                <label htmlFor="city">City:</label>
+                <input
+                  name="city"
+                  ref={register({ required: true })}
+                  placeholder="city:"
+                />
+                {errors.city && (
+                  <span className="ship-error">City is required</span>
+                )}
+              </div>
+              <div>
+                <label htmlFor="zipCode">Zip:</label>
+                <input
+                  name="zipCode"
+                  ref={register({ required: true })}
+                  placeholder="zipp code:"
+                />
+                {errors.zipCode && (
+                  <span className="ship-error">Zip Code is required</span>
+                )}
+              </div>
+            </li>
 
-          <li>
-            <input
-              name="zippCode"
-              ref={register({ required: true })}
-              placeholder="zippCode:"
-            />
-            {errors.zippCode && (
-              <span className="ship-error">Zipp Code is required</span>
-            )}
-          </li>
+            <li className="submit">
+              <button className="button primary" type="submit">
+                Submit
+              </button>
+            </li>
+          </ul>
+        </form>
+      </div>
 
-          <li>
-            <input
-              name="mobileNum"
-              ref={register({ required: true })}
-              placeholder="mobile number:"
-            />
-            {errors.mobileNum && (
-              <span className="ship-error">Mobile Number is required</span>
-            )}
-          </li>
-
-          <li className="submit">
-            <button className="button primary" type="submit">
-              Submit
-            </button>
-          </li>
-        </ul>
-      </form>
+      {/* PAYMENT FORM */}
+      <div
+        style={{ display: shipInfoAdded ? 'block' : 'none' }}
+        className="payment-container"
+      >
+        <h3>Payment Information</h3>
+        <div>
+          <Elements stripe={stripePromise}>
+            <CheckoutForm></CheckoutForm>
+          </Elements>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ frontend/src/components/checkoutForm/checkoutForm.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import './checkoutForm.css';
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [paymentError, setPaymentError] = useState(null);
+  const [paymentSuccess, setPaymentSuccess] = useState(null);
+
+  const [didMount, setDidMount] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+    if (error) {
+      setPaymentError(error.message);
+      setPaymentSuccess(null);
+    } else {
+      setPaymentSuccess(paymentMethod);
+      setPaymentError(null);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <ul className="payment-form">
+        <li>
+          <CardElement />
+        </li>
+        <li>
+          <button className="button primary" type="submit" disabled={!stripe}>
+            Pay
+          </button>
+        </li>
+        <li>
+          {paymentError && (
+            <small className="payment-error">{paymentError}</small>
+          )}
+          {paymentSuccess && (
+            <small className="payment-success">Payment Successfull</small>
+          )}
+        </li>
+      </ul>
+    </form>
+  );
+};
+export default CheckoutForm;
